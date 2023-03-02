072ee4c13952edc7871cf7d44a89de419e7e99b0
ANPHATPC\ADMIN <minhttgch210293@fpt.edu.vn>
2023-03-03 00:35:55 +0700

    feat account

--- src/app/accountant/edit-type-fee/edit-type-fee.component.ts
+++ src/app/accountant/edit-type-fee/edit-type-fee.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-type-fee.component.css']
 })
 export class EditTypeFeeComponent {
+  ngOnInit(): void {
+   
+  }
 
+  ngFilterYear = [2023,2022,2021,2020,2019,2018,2017,2016,2015,2014];
+  ngDropdownYear?= this.ngFilterYear[0];
+
+  
 }
--- src/app/accountant/fee-verification/fee-verification.component.ts
+++ src/app/accountant/fee-verification/fee-verification.component.ts
@@ -14,4 +14,13 @@ export class FeeVerificationComponent implements OnInit {
 
   ngFilterStatus = ["Tất cả", "Đã nộp đầy đủ phí ", "Thiếu phí giữ chỗ ", "Thiếu phí xét tuyển ", "Chưa nộp phí"];
   ngDropdownStatus?= "Tất cả";
+
+  ngFilterSemester = ["fall", "spring", "summer"];
+  ngDropdownSemester?= "all";
+
+  ngFilterYear = [2023,2022,2021,2020,2019,2018,2017,2016,2015,2014];
+  ngDropdownYear?= this.ngFilterYear[0];
+
+  ngFilterName = ["Nguyễn Văn A", "Nguyễn Văn B", "Nguyễn Văn C"];
+  ngDropdownName?= "Nguyễn Văn A";
 }
